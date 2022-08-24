8989d00b58118e6078dbf5fd5b0570baf15118fe
ayaznoori <ayaz.noori15@gmail.com>
2022-08-24 12:36:53 +0530

    bug solved

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import './App.css';
 import Navbar from './components/Navbar';
 import {Routes,Route} from 'react-router-dom';
-import Login from './pages/login/Login';
 import Contact from './pages/contacts/Contact';
 import Homepage from './pages/homepage/Homepage';
 import Pricing from './pages/pricing/Pricing';
 import Dashboard from './pages/dashboard/Dashboard';
 import Footer from './components/Footer';
+import Login from './pages/Register/Login/Login';
+import Signup from './pages/Register/Signup/Signup';
 
 function App() {
   return (
@@ -16,6 +17,7 @@ function App() {
               <Route path='/' element={<Homepage/>}/>
               <Route path='/dashboard/:user' element={<Dashboard/>}/>
               <Route path='/login' element={<Login/>}/>
+              <Route path='/signup' element={<Signup/>}/>
               <Route path='/contact' element={<Contact/>}/>
               <Route path='/Pricing' element={<Pricing/>}/>
           </Routes>
