c9b92d66edde608339f628f882327a12f8340554
ayaznoori <ayaz.noori15@gmail.com>
2022-08-24 09:46:07 +0530

    structure

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import './App.css';
 import Navbar from './components/Navbar';
 import {Routes,Route} from 'react-router-dom';
-import Login from './pages/Login';
-import Contact from './pages/Contact';
-import Homepage from './pages/Homepage';
-import Pricing from './pages/Pricing';
-import Dashboard from './pages/Dashboard';
+import Login from './pages/login/Login';
+import Contact from './pages/contacts/Contact';
+import Homepage from './pages/homepage/Homepage';
+import Pricing from './pages/pricing/Pricing';
+import Dashboard from './pages/dashboard/Dashboard';
 import Footer from './components/Footer';
 
 function App() {
