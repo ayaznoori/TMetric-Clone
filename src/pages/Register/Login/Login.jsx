9f606ff0274399802cc75e9abe757dbb59149696
Sagar Pawar <100870856+SPinfluencers@users.noreply.github.com>
2022-08-25 15:26:06 +0530

    login page

--- src/pages/Register/Login/Login.jsx
+++ src/pages/Register/Login/Login.jsx
@@ -21,20 +21,20 @@ const Login = () => {
          <div className={style.loginContainer_3}>
          <FormControl >
 
-            <FormLabel fontSize='xs'>Email</FormLabel>
-            <Input borderRadius={5} placeholder='johnsmith@gmail.com' marginBottom={5} size='xs' type='email' />
-            <FormLabel fontSize='xs'>Password</FormLabel>
-            <Input borderRadius={5} placeholder='Enter your password' marginBottom={5} size='xs' type='password' />
-            <Button padding='4' width={320} size='xs' color='white' marginBottom={4} backgroundColor='rgb(48,112,240)'>Button</Button>
+            <FormLabel color='rgb(163,126,133)' fontSize='xs'>Email</FormLabel>
+            <Input borderRadius={3} placeholder='johnsmith@gmail.com' marginBottom={3} size='xs' type='email' />
+            <FormLabel color='rgb(163,126,133)' fontSize='xs'>Password</FormLabel>
+            <Input borderRadius={3} placeholder='Enter your password' marginBottom={5} size='xs' type='password' />
+            <Button borderRadius={3} width={330} size='xs' color='white' marginBottom={4} backgroundColor='rgb(48,112,240)'>Log in</Button>
             <div className={style.lineText}><p>or</p></div>
 
-            <Button padding='4' width={320} size='xs' color='gray' marginBottom={4} colorScheme='gray'>Log in the Google</Button>
-            <Button padding='4' width={320} size='xs' color='gray' marginBottom={4} colorScheme='gray'>Log in the Microsoft</Button>
-            <Button padding='4' width={320} size='xs' color='gray' marginBottom={4} colorScheme='gray'>Log in the Apple</Button>
+            <Button variant='outline' borderRadius={3} width={330} size='xs' color='gray' marginBottom={4} colorScheme='gray'> <span><img width={15} src="https://w7.pngwing.com/pngs/249/19/png-transparent-google-logo-g-suite-google-guava-google-plus-company-text-logo.png" alt="google" /></span> Log in the Google</Button>
+            <Button variant='outline' borderRadius={3} width={330} size='xs' color='gray' marginBottom={4} colorScheme='gray'> <span><img width={10} src="https://companieslogo.com/img/orig/MSFT-a203b22d.png?t=1633073277" alt="microsoft" /></span> Log in the Microsoft</Button>
+            <Button variant='outline' borderRadius={3} width={330} size='xs' color='gray' marginBottom={4} colorScheme='gray'> <span><img width={15} src="https://assets.stickpng.com/images/580b57fcd9996e24bc43c516.png" alt="apple" /></span> Log in the Apple</Button>
             <hr />
             <div className={style.bottomSlide}>
-              <a href="">Can't Log In?</a>
-              <a href="">Sign Up</a>
+              <a href=""><Text>Can't Log In?</Text></a>
+              <a href=""><Text>Sign Up</Text></a>
             </div>
           </FormControl>
           </div>
