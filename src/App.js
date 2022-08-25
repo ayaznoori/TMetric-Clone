fd8ebb4a82e80c0d4b8d27be31d0810b1e613714
DeveloperShubhamKapoor <kapoorshubham483@gmail.com>
2022-08-25 22:35:59 +0530

    added teampage structure and json data

--- src/App.js
+++ src/App.js
@@ -1,27 +1,27 @@
-import './App.css';
-import Navbar from './components/Navbar';
-import {Routes,Route} from 'react-router-dom';
-import Contact from './pages/contacts/Contact';
-import Homepage from './pages/homepage/Homepage';
-import Pricing from './pages/pricing/Pricing';
-import Dashboard from './pages/dashboard/Dashboard';
-import Footer from './components/Footer';
-import Login from './pages/Ragister/Login/Login';
-import Team from './components/Team';
+import "./App.css";
+import Navbar from "./components/Navbar";
+import { Routes, Route } from "react-router-dom";
+import Contact from "./pages/contacts/Contact";
+import Homepage from "./pages/homepage/Homepage";
+import Pricing from "./pages/pricing/Pricing";
+import Dashboard from "./pages/dashboard/Dashboard";
+import Footer from "./components/Footer";
+import Login from "./pages/Ragister/Login/Login";
+import Team from "./components/Team";
 
 function App() {
   return (
     <div className="App">
-          <Navbar/>
-          <Routes>
-              <Route path='/' element={<Homepage/>}/>
-              <Route path='/dashboard/:user' element={<Dashboard/>}/>
-              <Route path='/login' element={<Login/>}/>
-              <Route path='/contact' element={<Contact/>}/>
-              <Route path='/Pricing' element={<Pricing/>}/>
-          </Routes>
-          <Footer/>
-          <Team/>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Homepage />} />
+        <Route path="/dashboard/:user" element={<Dashboard />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/Pricing" element={<Pricing />} />
+      </Routes>
+      <Footer />
+      <Team />
     </div>
   );
 }
