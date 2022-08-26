87fb8cc04713c42e7871ddec60c86eace40f5ba3
shashankkumarP <nawalerason@gmail.com>
2022-08-26 09:46:12 +0530

    Sshankfriday

--- src/pages/dashboard/Task/Add.jsx
+++ src/pages/dashboard/Task/Add.jsx
@@ -1,19 +1,19 @@
-import { HStack,Image } from '@chakra-ui/react'
+import { HStack,Image,Input } from '@chakra-ui/react'
 import React from 'react'
 
-const Add = () => {
+const Add = ({handlechange}) => {
 
 
   return (
     <div>
         <HStack justifyContent={'space-around'}  >
-            <HStack gap='10px' >
+            <HStack gap='20px' pd='0.1px' >
                 <Image w='20px' h='20px' src='https://cdn3.iconfinder.com/data/icons/sympletts-free-sampler/128/circle-check-512.png' />
-                <Input variant='flushed' placeholder='' />
+                <Input variant='flushed' name='name' onChange={handlechange}  placeholder='' />
 
             </HStack>
             <HStack gap='10px'  >
-                <Image  />
+                <Image src='https://www.kindpng.com/picc/m/24-248325_profile-picture-circle-png-transparent-png.png' h='20px' w='20px' />
                 <span><i class="fa-solid fa-play"></i></span>
             </HStack>
         </HStack>
--- src/pages/dashboard/Task/Task.jsx
+++ src/pages/dashboard/Task/Task.jsx
@@ -1,4 +1,4 @@
-import React,{useEffect,useRef} from 'react'
+import React,{useEffect,useRef, useState} from 'react'
 import { Button, Stack ,HStack} from '@chakra-ui/react'
 import {
   Menu,
@@ -14,11 +14,96 @@ import {
 
 import {ChevronDownIcon,SearchIcon,Icon} from "@chakra-ui/icons"
 import Menu_component from './Menu_component';
+import Add from './Add';
+import Work from "./Work"
 let menu=["Client","Project","Status","Create","Souce"]
 
+let handlefun = (a)=>{
+
+return (
+  <div>
+               <HStack display={'flex'} justifyContent='space-between' >
+                  <HStack pl='5px' >
+                    <span><Image src='https://previews.123rf.com/images/elnurss/elnurss1809/elnurss180903233/109840299-play-button-vector-icon.jpg?fj=1' w='50px' h='50px' /></span>
+                    <Button><i class="fa-solid fa-check"></i> &nbsp; Mark as completed</Button>
+                  </HStack>
+                  <HStack gap='5px' m='2px' >
+                    <span><Image src='https://icon-library.com/images/more-icon/more-icon-25.jpg' w='30px' h='30px' /></span>
+                    <span><Image src='https://icons.veryicon.com/png/o/commerce-shopping/online-retailers/arrow-right-33.png' w='30px' h='30px' /></span>
+                  </HStack>
+                </HStack>
+                <Stack><Input placeholder='Write a Task Name' value={a} /></Stack>
+                <HStack>
+                  <h3>Projects</h3>
+                  <HStack pl='25px' >
+                    <Image w='10px' h='10px' src='https://d1nhio0ox7pgb.cloudfront.net/_img/o_collection_png/green_dark_grey/512x512/plain/folder.png' /> 
+                    <p>No Projects</p>
+                  </HStack> 
+
+                </HStack>
+                <HStack gap='25px' >
+                  <p>Assignee</p>
+                  <h4>Shashank Kumar</h4>
+                </HStack>
+                <HStack>
+                  <h4>Due Date</h4>
+                  <HStack pl='25px' >
+                    <Image src='https://cdn5.vectorstock.com/i/1000x1000/51/09/calendar-icon-vector-22895109.jpg' w='20px' h='20px' />
+                    <h4>None</h4>
+                  </HStack>
+                </HStack>
+                <Stack><Input placeholder='Enter task Description' /></Stack>
+                <HStack>
+                  <h4>Estimate</h4>
+                  <HStack pl='25px' >
+                    <Image src='https://img.freepik.com/premium-vector/black-white-vector-illustration-vintage-analog-clock_97886-1.jpg?w=2000' w='20px' h='20px' />
+                    <p>None</p>
+                  </HStack>
+                </HStack>
+                <HStack>
+                <Image src='https://static.thenounproject.com/png/861915-200.png' w='20px' h='20px' />
+                <h4>Add Tag</h4>
+                </HStack>
+                <HStack>
+                <Image src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR-OXh5xmCjjEFuWkrLzSAQc_4F-X4t5jKF0Q&usqp=CAU' w='20px' h='20px' />
+                <h4>Activity</h4>
+                </HStack>
+        </div>
+)
+}
+
 const Task = () => {
+  let [users,setUsers] = useState([]);
+  let [work,setWork] = useState([]);
+  let [wrk,setWrk] = useState({
+    name:""
+  });
+  let ref= useRef(null);
+  let [s,setS] = useState(false); 
 
+  let handleclick=()=>{
+    // if(ref.current!=null){
+    //   let a= handlefun(wrk.name);
+    //   setUsers([...users,])
+    // }
+    // ref.current=null;
+    f();
+    setS(false)
+    
+  }
+  let f = ()=>{
+    return (
+      <Add handlechange={handlechange} />
+    )
 
+  }
+  let handlechange= (e)=>{
+    let {name,value} = e.target;
+    setWrk({...wrk,[name]:value})
+    console.log(wrk);
+    ref.current=e.target.value;
+  }
+  
 
   return (
     <div w='80%' >
@@ -39,7 +124,7 @@ const Task = () => {
           <HStack justifyContent={'space-between'} alignItems={'center'} h='70px' pl='5px' borderRadius={"10px"}  boxShadow=' rgba(0, 0, 0, 0.02) 0px 1px 3px 0px, rgba(27, 31, 35, 0.15) 0px 0px 0px 1px' >
             
             <HStack  >
-              <Button  bg='blue' color='white' ><i class="fa-solid fa-plus"></i> &nbsp; New Tak</Button>
+              <Button onClick={()=>setS(true)} _hover={{bg:"x"}}  bg='blue' color='white' ><i class="fa-solid fa-plus"></i> &nbsp; New Tak</Button>
               <Menu_component name='Sort :Project' />
             </HStack>
             <Stack>
@@ -48,55 +133,13 @@ const Task = () => {
           </HStack>
           <HStack >
             <Stack w='50%' h='100%' border='1px solid'  >
+              {/* <Add handlechange={handlechange} /> */}
+              {s==true && f()}
               
             </Stack>
             <Stack  w='50%' h='100%' border='1px solid red' >
-              <HStack display={'flex'} justifyContent='space-between' >
-                <HStack pl='5px' >
-                  <span><Image src='https://previews.123rf.com/images/elnurss/elnurss1809/elnurss180903233/109840299-play-button-vector-icon.jpg?fj=1' w='50px' h='50px' /></span>
-                  <Button><i class="fa-solid fa-check"></i> &nbsp; Mark as completed</Button>
-                </HStack>
-                <HStack gap='5px' m='2px' >
-                  <span><Image src='https://icon-library.com/images/more-icon/more-icon-25.jpg' w='30px' h='30px' /></span>
-                  <span><Image src='https://icons.veryicon.com/png/o/commerce-shopping/online-retailers/arrow-right-33.png' w='30px' h='30px' /></span>
-                </HStack>
-              </HStack>
-              <Stack><Input placeholder='Write a Task Name' /></Stack>
-              <HStack>
-                <h3>Projects</h3>
-                <HStack pl='25px' >
-                  <Image w='10px' h='10px' src='https://d1nhio0ox7pgb.cloudfront.net/_img/o_collection_png/green_dark_grey/512x512/plain/folder.png' /> 
-                  <p>No Projects</p>
-                </HStack> 
-
-              </HStack>
-              <HStack gap='25px' >
-                <p>Assignee</p>
-                <h4>Shashank Kumar</h4>
-              </HStack>
-              <HStack>
-                <h4>Due Date</h4>
-                <HStack pl='25px' >
-                  <Image src='https://cdn5.vectorstock.com/i/1000x1000/51/09/calendar-icon-vector-22895109.jpg' w='20px' h='20px' />
-                  <h4>None</h4>
-                </HStack>
-              </HStack>
-               <Stack><Input placeholder='Enter task Description' /></Stack>
-              <HStack>
-                <h4>Estimate</h4>
-                <HStack pl='25px' >
-                  <Image src='https://img.freepik.com/premium-vector/black-white-vector-illustration-vintage-analog-clock_97886-1.jpg?w=2000' w='20px' h='20px' />
-                  <p>None</p>
-                </HStack>
-              </HStack>
-              <HStack>
-              <Image src='https://static.thenounproject.com/png/861915-200.png' w='20px' h='20px' />
-              <h4>Add Tag</h4>
-              </HStack>
-              <HStack>
-              <Image src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR-OXh5xmCjjEFuWkrLzSAQc_4F-X4t5jKF0Q&usqp=CAU' w='20px' h='20px' />
-              <h4>Activity</h4>
-              </HStack>
+              {/* {handlefun()} */}
+              <Work wrk={`${wrk.name}`} />
             </Stack>
           </HStack>
         </Stack>
